fdc8ccf9ae127f2829e18fe955d249509e127cdd
Collin Rea <collinr01@gmail.com>
2017-02-01 12:21:17 -0600

    Bug Fix: fixed issue where refreshing on error show page after registration the app would crash, redirected to users/new

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   resources :restaurants
   resources :users, only: [:new, :create, :show, :edit]
 
+  get '/users' => redirect('/users/new')
+
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   delete '/logout' => 'sessions#destroy', :via => :delete
