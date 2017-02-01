67fa4a0c1e40842ad5065773857e44c4c561e32b
Collin Rea <collinr01@gmail.com>
2017-02-01 14:20:14 -0600

    Add table to display restaurants + average method to give ratings for each restaurant

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -4,4 +4,11 @@ class Restaurant < ApplicationRecord
   validates :name, presence: true
   validates_uniqueness_of :name
 
+  def average
+    ratings = Rating.where(id: self.id)
+    if ratings.length > 0
+      total = ratings.reduce(0) {|total, rating| total + rating.score}
+      total / ratings.length
+    end
+  end
 end
