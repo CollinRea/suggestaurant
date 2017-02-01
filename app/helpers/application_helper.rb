be3188d2c1405019af5e010f81230bd47aa709a9
Collin Rea <collinr01@gmail.com>
2017-02-01 10:32:17 -0600

    Add Layout Nav menu an application helper for current_user and logged_in?

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def logged_in?
+    return true if session[:user_id]
+  end
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if logged_in?
+  end
 end
