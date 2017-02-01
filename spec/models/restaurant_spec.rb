6056a0cdf528f0f1a788e58a0652ab58bd785544
Collin Rea <collinr01@gmail.com>
2017-02-01 08:38:31 -0600

    Add validation tests for Restaurant model

--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Restaurant, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let("restaurant") {Restaurant.new()}
+
+  describe 'Validations' do
+    it 'is not valid without a name' do
+      expect(restaurant).to_not be_valid
+    end
+
+    it 'is valid with a name' do
+      restaurant.name = "Joe's Sub Shop"
+      expect(restaurant).to be_valid
+    end
+  end
 end
