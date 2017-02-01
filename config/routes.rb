f47335e3940cfbb7eb5ed3f19842b52283725f92
Collin Rea <collinr01@gmail.com>
2017-02-01 12:00:35 -0600

    Add user registration, user login/logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
 
   resources :restaurants
+  resources :users, only: [:new, :create, :show, :edit]
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  delete '/logout' => 'sessions#destroy', :via => :delete
 
   root controller: 'application', action: 'index'
-
 end
