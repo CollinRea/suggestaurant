4b3b6adb1161265e849a1c6545bbe5011483e3d0
Collin Rea <collinr01@gmail.com>
2017-02-01 08:21:47 -0600

    Add validation tests for User model

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -2,7 +2,7 @@ class Rating < ApplicationRecord
   validates :user_id, presence: true
   validates :restaurant_id, presence: true
   validates_numericality_of :score, only_integer: true,
-                                    greater_than_or_equal_to: 0:,
+                                    greater_than_or_equal_to: 0,
                                     less_than_or_equal_to: 5
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,48 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe 'Validations' do
+    it 'is not valid without a name' do
+      user = User.new(password: '123456', email: 'test@test.com')
+      expect(user).to_not be_valid
+    end
+
+    it 'is not valid without an email' do
+      user = User.new(name: 'Collin', password: 'test123')
+      expect(user).to_not be_valid
+    end
+
+    it 'must have a properly formatted email' do
+      user = User.new(name: 'Collin', password: 'test123', email: 'test.com')
+      expect(user).to_not be_valid
+    end
+
+    it 'must have a unique email' do
+      user1 = User.create(name: 'Collin', password: 'test123', email: 'test@test.com')
+      user2 = User.new(name: 'Justin', password: 'newpass12', email: 'test@test.com')
+      expect(User.exists?(name: 'Collin')).to be true
+      expect(user2).to_not be_valid
+    end
+
+    it 'is not valid without a password' do
+      user = User.new(name: 'Collin', email: 'test@test.com')
+      expect(user).to_not be_valid
+    end
+
+    it 'must have a password greater than 6 characters' do
+      user = User.new(name: 'Collin', email: 'test@test.com', password: '123')
+      expect(user).to_not be_valid
+    end
+
+    it 'should not save raw password to database' do
+      user = User.new(name: 'Collin', email: 'test@test.com', password: 'test123')
+      expect(user.password).to eq 'test123'
+    end
+
+    it 'is valid with a name, email, and password' do
+      user = User.new(name: 'Collin', email: 'test@test.com', password: 'test123')
+      expect(user).to be_valid
+    end
+  end
 end
