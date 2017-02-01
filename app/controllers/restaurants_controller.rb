75a4b3f63c25ff946d0a55ab08fbf7076fcd47b0
Collin Rea <collinr01@gmail.com>
2017-02-01 13:51:55 -0600

    Finish controller to create new restaurant

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -4,4 +4,24 @@ class RestaurantsController < ApplicationController
     @restaurants = Restaurant.all
   end
 
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      redirect_to restaurants_path
+    else
+      @errors = restaurant.errors.full_messages
+      render :new
+    end
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :category, :last_visited)
+  end
+
 end
