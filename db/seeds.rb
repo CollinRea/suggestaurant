9645683e0a1cba3327327c60f67307d474127338
Collin Rea <collinr01@gmail.com>
2017-02-01 09:44:17 -0600

    Add seed data to populate DB with small sample of restaurants

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+restaurants = [
+  {name: 'Subway', category: 'Sandwiches', last_visited: Date.parse('1/1/2017')},
+  {name: 'Jimmy Johns', category: 'Sandwiches', last_visited: Date.parse('2/1/2017')},
+  {name: 'Nan Thai', category: 'Thai', last_visited: Date.parse('25/1/2017')},
+  {name: 'Antico', category: 'Pizza', last_visited: Date.parse('30/12/2016')},
+  {name: 'Lure', category: 'Seafood', last_visited: Date.parse('22/12/2016')},
+  {name: 'Rosa Mexicano', category: 'Mexican', last_visited: Date.parse('28/11/2016')}
+]
+
+restaurants.each do |restaurant|
+  Restaurant.create(restaurant)
+end
